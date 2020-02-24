fb29af9235afd21412b670a2bc7dc775b5eab0f3
Fabio Di Matteo <pobfdm@gmail.com>
2020-02-24 12:50:03 +0100

    Fix dirPicker problem

--- web/pages/home.php
+++ web/pages/home.php
@@ -8,6 +8,12 @@
   
   <p class="text-justify mt-2">Pobshare is a simple and pleasant graphical interface to quickly share files and folders in the local network. It has several options to ensure safety and is suitable for beginners.</p>
   <a class="btn btn-success btn-lg" href="index.php?page=downloads" role="button">Download !</a>
+
+
+	<div class="alert alert-info" role="alert" style="margin-top:20px">
+	  <h3>News: </h3>
+	  <p>2019-03-21 Added support for FTPS in 0.2</p>
+	</div>
 </div>
 
 
